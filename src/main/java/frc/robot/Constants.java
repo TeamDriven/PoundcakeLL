744c4df739d0a546b3bd982339e0c280d4816e23
Jay <44783760+jschenke488@users.noreply.github.com>
2026-01-16 15:39:16 -0600

    shooter and angle controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,14 @@ public class Constants {
     // Intake
 
     // Pivot
-    public static final double REST_POS = 0; // TODO find the actual value
+    public static final double PIVOT_REST_POS = 0; // TODO find the actual value
     public static final double pivotInternalTicksPerDegree = 1; // TODO find the actual value
     public static final double pivotTicksPerDegree = 1; // TODO find the actual value
     public static final double pivotOffset = 1; // TODO find the actual value
+
+    // Shooter
+
+    // AngleController
+    public static final double ANGLE_CONTROLLER_REST_POS = 0;
+    public static final double GEAR_RATIO = 1;
 }
