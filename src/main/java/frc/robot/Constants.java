79dd196025ef176d3a3b23b76e19f835820cde48
zoeysears <53350958+zoeysears@users.noreply.github.com>
2026-03-27 19:02:15 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,7 +11,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Constants {
     public class DrivetrainConst {
-        public static final double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+        public static final double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.5; // kSpeedAt12Volts desired top speed
         public static final double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
     }
 
